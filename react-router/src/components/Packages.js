23f71ff390d430e93a88337f345b16950898b130
Chris Wehner <chrisw7128@gmail.com>
2023-05-20 11:43:44 -0700

    first commit

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,15 +1,13 @@
-
-
-export default function Packages() {
-
-    return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                
-            </div>
+export default function Packages({ packages }) {
+  const displayPackages = packages.map(p => <li>{p}</li>);
+  return (
+    <div>
+      <div className="packages">
+        <div className="packagesHeader">
+          <h1>Our Packages</h1>
         </div>
-    )
+        <ul>{displayPackages}</ul>
+      </div>
+    </div>
+  );
 }
